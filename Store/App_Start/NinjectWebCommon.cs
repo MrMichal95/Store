7eedf8bd1581ff62ef5274644647577cf1ceb3b9
Michal <michal.schmidt95@gmail.com>
2017-02-24 22:55:20 +0100

    NinjectDependencyResolver setup as default dependency resolver with MVC

--- Store/App_Start/NinjectWebCommon.cs
+++ Store/App_Start/NinjectWebCommon.cs
@@ -1,3 +1,5 @@
+using Store.Infrastructure;
+
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Store.App_Start.NinjectWebCommon), "Start")]
 [assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(Store.App_Start.NinjectWebCommon), "Stop")]
 
@@ -61,6 +63,7 @@ namespace Store.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System.Web.Mvc.DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
         }        
     }
 }
