3942031b69db161317106cef75a87e38a2312fb0
Michal <michal.schmidt95@gmail.com>
2017-02-24 22:51:07 +0100

    NinjectDependencyResolver

---
+++ Store/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace Store.Infrastructure
+{
+    public class NinjectDependencyResolver: IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            this._kernel = kernel;
+            AddBindings();
+        }
+        
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            
+        }
+    }
+}
