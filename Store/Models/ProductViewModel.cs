6ad8fd32cc8477c267042932ffbf387c10fcaf0f
Michal <michal.schmidt95@gmail.com>
2017-03-04 23:54:52 +0100

    added ProductViewModel

---
+++ Store/Models/ProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using Store.Domain.Entities;
+
+namespace Store.Models
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
